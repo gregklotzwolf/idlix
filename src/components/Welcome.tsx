1b12c1b57439087fbd33f570ea4810a7425a3f0c
gregklotzwolf <130932618+gregklotzwolf@users.noreply.github.com>
2023-04-17 04:55:21 +0800

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,7 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Halo, Aku AI IDLIX</h1>
     <p>
-      Dengan AI ini kamu dapat bertanya seputar rekomendasi film terkini, kamu bisa apa saja misalnya contoh: "rekomendasi film horror 2023" atau "rekomendasi film action 2019" dll. Apapun seputar film bisa kamu tanya AI kami. Gunakan bahasa Indonesia dengan perintah "Bahasa Indonesia" nikmatilah teknologi kami. Jangan lupa ⭐ 5 pada Google Play. 
+      Dengan AI ini kamu dapat bertanya seputar rekomendasi film terkini, kamu bisa bertanya apa saja seputar film misalnya: "rekomendasi film horror 2023" atau "rekomendasi film action 2019" dll. Apapun seputar film bisa kamu tanya pada AI kami. Gunakan bahasa Indonesia dengan perintah "Bahasa Indonesia" nikmatilah teknologi kami. Jangan lupa ⭐ 5 pada Google Play. 
     </p>
     <p>
       <a className="underline" href="https://play.google.com/store/apps/details?id=com.idlix.rdpdigitalkreatif">
